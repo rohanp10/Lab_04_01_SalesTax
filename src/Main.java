f02a10d64d21f0d5a5a5e228b7383e8d6c406096
rohanparikh <parikhrs@mail.uc.edu>
2023-06-12 20:27:48 -0400

    Basic Working Code

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,14 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        
+
+        double purchasePrice = 10.80;
+        double salesTax = 0.0;
+
+        salesTax = purchasePrice * 0.05;
+
+        System.out.println("The price of the purchase is $" + purchasePrice);
+        System.out.println("The sales tax for the purchase is $" + salesTax);
+
     }
 }
